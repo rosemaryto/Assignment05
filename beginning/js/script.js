82cb4efdfcde1cc1458bfd683f4da6071f9ccadf
rosemaryto <rosemary_to@ymail.com>
2021-03-16 21:03:50 -0700

    Done

--- beginning/js/script.js
+++ beginning/js/script.js
@@ -1,3 +1,16 @@
-window.addEventListener('load', function() {
+
+
+
+window.addEventListener('load', () => {
     "use strict";
+var ideInput = document.getElementById('ide');
+var nameInput = document.getElementById('nam');
+var extInput = document.getElementById('ext');
+var emaInput = document.getElementById('ema');
+var depInput = document.getElementById('dep');
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log('ID: ' + ideInput.value + '\n' + 'Name:' + nameInput.value + '\n' +  'Extension:' + extInput.value + '\n' + 'Email:' + emaInput.value +  '\n' + 'Department:' + depInput.value);
+});
+
 });
